3304cb4c4105fc27065e41a5e9af3556eed01dd3
Darius Liddell <darius@carerev.com>
2024-11-16 19:19:49 +0100

    unfold for iterate and binaryTree

--- chapterTwelve.hs
+++ chapterTwelve.hs
@@ -137,11 +137,55 @@ module ChapterTwelve where
 Write the function myIterate using direct recursion.
 Compare the behavior with the built-in iterate to gauge correctness.
 Do not look at the source or any examples of iterate so that you are forced to do this yourself.
+
+-- iterate is like a very limited
+-- unfold that never ends
+Prelude> :t iterate
+iterate :: (a -> a) -> a -> [a]
+-- because it never ends, we must use
+-- take to get a finite list
+Prelude> take 10 $ iterate (+1) 0
+[0,1,2,3,4,5,6,7,8,9]
+
+-- unfoldr is the full monty
+Prelude> :t unfoldr
+unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+-- Using unfoldr to do the same as thing as iterate
+Prelude> take 10 $ unfoldr (\b -> Just (b, b+1)) 0
+[0,1,2,3,4,5,6,7,8,9]
+
 --}
 
   myIterate :: (a -> a) -> a -> [a]
   myIterate f a' = a' : myIterate f (f a')
 
-
+  
   myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-  myUnfoldr f b' = undefined 
+  myUnfoldr f b' = a' : restOfList where
+    (a', b'') = fromJust $ f b'
+    restOfList = myUnfoldr f b''
+{--
+  Rewrite myIterate into betterIterate using myUnfoldr.
+  A hint – we used unfoldr to produce the same results as iterate earlier.
+  Do this with different functions and see if you can abstract the structure out.
+  It helps to have the types in front of you
+  myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+--}
+  betterIterate :: (a -> a) -> a -> [a]
+  betterIterate f = myUnfoldr (\b -> Just (b,f b))
+
+
+  data BinaryTree a = Leaf
+    | Node (BinaryTree a)
+    a
+    (BinaryTree a) deriving (Eq, Ord, Show)
+
+-- Write unfold for BinaryTree.
+-- aka an anamorphism not a catamorphism
+-- unfolding is turning an arbitrary value (list, num, hashmap, set, etc) into a binary tree
+  unfold :: (a -> Maybe (a,b,a)) -> a -> BinaryTree b
+  unfold f a' = case f a' of
+                Nothing            -> Leaf
+                Just (l, root, r)  -> Node left root right where
+                  left = unfold f l
+                  right = unfold f r
