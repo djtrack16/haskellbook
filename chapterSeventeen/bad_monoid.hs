ab0af3ac20108228ddae8c0bb3fa9e478c78d698
Darius Liddell <darius@carerev.com>
2024-11-22 18:08:02 +0100

    list worign but not ziplist

--- chapterSeventeen/bad_monoid.hs
+++ chapterSeventeen/bad_monoid.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Eta reduce" #-}
 module BadMonoid where
   import Data.Monoid qualified as M
   import Data.Semigroup qualified as S
@@ -25,58 +27,6 @@ module BadMonoid where
 
   --instance EqProp Bull where
   --  (=-=) = eq
-
--- ZIPLIST MONOID
-
-  instance Monoid a => Monoid (ZipList a) where
-    mempty = pure mempty
-
-  instance Semigroup a => S.Semigroup (ZipList a) where
-    (<>) = liftA2 (S.<>)
-
-
-
-  --instance Arbitrary a => Arbitrary (ZipList a) where
-  --  arbitrary = ZipList <$> arbitrary
-
-  --instance Arbitrary a => Arbitrary (Sum a) where
-   -- arbitrary = Sum <$> arbitrary
-
-  --instance Eq a => EqProp (ZipList a) where
-    --(=-=) = eq
-
-{--
-Implement the List Applicative. Writing a minimally complete Ap- plicative instance calls for
-writing the definitions of both pure and <*>. We’re going to provide a hint as well.
-Use the checkers library to validate your Applicative instance.
-
-Prelude> let functions = Cons (+1) (Cons (*2) Nil)
-Prelude> let values = Cons 1 (Cons 2 Nil)
-Prelude> functions <*> values
-Cons 2 (Cons 3 (Cons 2 (Cons 4 Nil)))
-
-    (<*>) :: Identity (a -> b) -> Identity a -> Identity b
-    (<*>) (Identity f) (Identity a') = Identity (f a')
-
---}
-
-  data List a =
-    Nil
-    | Cons a (List a)
-    deriving (Eq, Show)
-
-  instance Functor List where
-    fmap = undefined
-
-  instance Applicative List where
-    pure :: a -> f a
-    pure = undefined--f
-
-    (<*>) :: List (a -> b) -> List a -> List b
-    (<*>) Nil Nil  = Nil
-    --(<*>) (Cons a list) Nil  = Cons a Nil <*> list
-
-    --(<*>) f (Cons a list) = Cons (f a) (f <*> list)
 
   main :: IO ()
   main = do
---
+++ chapterSeventeen/list.hs
@@ -0,0 +1,142 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use <$>" #-}
+{-# LANGUAGE MonoLocalBinds #-}
+module List where
+
+  import Data.Monoid qualified as M
+  import Data.Semigroup qualified as S
+
+  import Test.QuickCheck
+  import Test.QuickCheck.Checkers
+  import Test.QuickCheck.Classes
+  import Control.Applicative
+
+  take' :: Int -> List a -> List a
+  take' = undefined
+
+  -- ZIPLIST MONOID
+
+  newtype ZipList' a =
+    ZipList' (List a)
+    deriving (Eq, Show)
+
+  instance Eq a => EqProp (ZipList' a) where
+    xs =-= ys = xs' `eq` ys'
+      where xs' = let (ZipList' l) = xs
+                  in take' 3000 l
+            ys' = let (ZipList' l) = ys
+                  in take' 3000 l
+
+  instance Functor ZipList' where
+    fmap :: (a -> b) -> ZipList' a -> ZipList' b
+    fmap f (ZipList' xs) = ZipList' (fmap f xs)
+
+  instance Applicative ZipList' where
+
+    pure :: a -> ZipList' a
+    pure a = ZipList' (Cons a Nil)
+
+
+    (<*>) :: ZipList' (a -> b) -> ZipList' a -> ZipList' b
+    (<*>) (ZipList' Nil) (ZipList' list) = ZipList' Nil
+    (<*>) (ZipList' list) (ZipList' Nil) = ZipList' Nil
+    (<*>) (ZipList' (Cons f list)) (ZipList' (Cons a list')) = ZipList' (Cons (f a) (list <*> list'))
+
+
+
+  instance (Arbitrary a) => Arbitrary (ZipList (List a)) where
+    arbitrary = do
+      a <- arbitrary
+      frequency [
+        (1, return (ZipList' Nil)) ,
+        (1, return ZipList' (Cons <$> arbitrary <*> arbitrary))
+        ]
+
+    --arbitrary = frequency [ (1, return (ZipList' Nil)) , (1, return Twoo) ]
+{--
+
+instance Arbitrary a => Arbitrary (Tree a) where
+arbitrary = sized arbTree
+
+arbTree :: Arbitrary a => Int -> Gen (Tree a)
+arbTree 0 = do
+    a <- arbitrary
+    return $ Node a []
+arbTree n = do
+    (Positive m) <- arbitrary
+    let n' = n `div` (m + 1)
+    f <- replicateM m (arbTree n')
+    a <- arbitrary
+    return $ Node a f
+
+Implement the List Applicative. Writing a minimally complete Ap- plicative instance calls for
+writing the definitions of both pure and <*>. We’re going to provide a hint as well.
+Use the checkers library to validate your Applicative instance.
+
+Prelude> let functions = Cons (+1) (Cons (*2) Nil)
+Prelude> let values = Cons 1 (Cons 2 Nil)
+Prelude> functions <*> values
+Cons 2 (Cons 3 (Cons 2 (Cons 4 Nil)))
+
+
+--}
+
+  data List a =
+    Nil
+    | Cons a (List a)
+    deriving (Eq, Show)
+
+  instance Functor List where
+
+    fmap :: (a -> b) -> List a -> List b
+    fmap f Nil = Nil
+    fmap f (Cons a list) = Cons (f a) (fmap f list)
+
+  instance Applicative List where
+    pure :: a -> List a
+    pure a = Cons a Nil
+
+    (<*>) :: List (a -> b) -> List a -> List b
+    (<*>) (Cons f listF) listA = append (fmap f listA) (listF <*> listA)
+    (<*>) _ Nil = Nil
+    (<*>) Nil _ = Nil
+
+  instance (Arbitrary a) => Arbitrary (List a) where
+    arbitrary :: Arbitrary a => Gen (List a)
+    arbitrary = oneof [
+        return Nil,
+        Cons <$> arbitrary <*> arbitrary
+      ]
+
+  instance Eq a => EqProp (List a) where
+    (=-=) = eq
+
+  functions = Cons (+1) (Cons (*2) Nil)
+  values = Cons 1 (Cons 2 Nil)
+
+  testType = undefined :: (String, String, String)
+
+
+  append :: List a -> List a -> List a
+  append Nil ys = ys
+  append (Cons x xs) ys = Cons x $ xs `append` ys
+
+  fold :: (a -> b -> b) -> b -> List a -> b
+  fold _ b Nil = b
+  fold f b (Cons h t) = f h (fold f b t)
+
+  concat' :: List (List a) -> List a
+  concat' = fold append Nil
+
+  -- write this one in terms of concat' and fmap
+  flatMap :: (a -> List b) -> List a -> List b
+  flatMap f as = concat' $ fmap f as
+
+  exampleList :: List (String, String, String)
+  exampleList = Cons testType (Cons testType Nil)
+  
+
+  main :: IO()
+  main = do
+    quickBatch (applicative exampleList)
+    quickBatch (applicative (ZipList' exampleList))
